5b9eb01d4a5f058405726b334ed4cebbfc84a8fa
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-03-04 08:10:58 +0300

    code comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,37 +1,51 @@
 class ApplicationController < Sinatra::Base
+    # Set the default content type for all responses to be JSON.
     set :default_content_type, "application/json"
-
+  
+    # GET all tasks.
     get "/task" do
-        task = Task.all
-        task.to_json
+      # Get all tasks from the database.
+      task = Task.all
+      # Convert the task data to JSON format.
+      task.to_json
     end
-
+  
+    # GET a specific task by ID.
     get "/task/:id" do
-            task = Task.find(params[:id])
-            task.to_json
-          end
-
-          post "/task" do
-                tasks = Task.create(
-                activity: params[:activity]
-                
-                )
-                tasks.to_json
-            end
-
-             delete "/task/:id" do
-        tasks = Task.find(params[:id])
-        tasks.destroy
-        tasks.to_json
+      # Find the task with the given ID in the database.
+      task = Task.find(params[:id])
+      # Convert the task data to JSON format.
+      task.to_json
     end
-
+  
+    # POST a new task.
+    post "/task" do
+      # Create a new task with the activity provided in the request parameters.
+      tasks = Task.create(
+        activity: params[:activity]
+      )
+      # Convert the new task data to JSON format and return it.
+      tasks.to_json
+    end
+  
+    # DELETE a task by ID.
+    delete "/task/:id" do
+      # Find the task with the given ID in the database.
+      tasks = Task.find(params[:id])
+      # Destroy the task, removing it from the database.
+      tasks.destroy
+      # Convert the task data to JSON format and return it.
+      tasks.to_json
+    end
+  
+    # PATCH (update) a task by ID.
     patch "/task/:id" do
-        task = Task.find(params[:id])
-        task.update(activity: params[:activity])
-        task.to_json
-      end
-
-    
-      
-    
-end
+      # Find the task with the given ID in the database.
+      task = Task.find(params[:id])
+      # Update the activity of the task with the value provided in the request parameters.
+      task.update(activity: params[:activity])
+      # Convert the updated task data to JSON format and return it.
+      task.to_json
+    end
+  end
+  
